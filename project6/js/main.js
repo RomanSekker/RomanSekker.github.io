87f02baa2dc7fc24dc3a187b08ad8ef63751ea7d
Your Name <you@example.com>
2018-07-31 00:53:34 +0300

    4 commit

--- project6/js/main.js
+++ project6/js/main.js
@@ -32,6 +32,27 @@ $(document).ready(function() {
 	// 	"loop": true
 	// });
 
+
+
+	<script>
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "Play";
+    }
+}
+</script>
+
 	// end-bg-video
 
 
